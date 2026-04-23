9928700a157d3a802965b354a27cd7647d521c7e
DownloadableFox <me@downloadablefox.dev>
2026-04-23 00:12:57 -0500

    removed login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/Login"
-import Callback from "./pages/Callback"
 import GestionCenso from "./pages/GestionCenso"
 import ProtectedRoute from "./components/ProtectedRoute"
 
@@ -8,18 +6,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Ruta p�blica */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/callback" element={<Callback />} />
-
-        {/* Rutas protegidas � requieren JWT v�lido */}
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Navigate to="/censo/gestion" replace />} />
           <Route path="/censo/gestion" element={<GestionCenso />} />
         </Route>
-
-        {/* Catch-all */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   )
